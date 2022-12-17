a67652e1aaab2abe1f44ff565174437ae6227f4e
Georgiy Glinskikh <glnskkh@vk.com>
2022-12-17 16:33:14 +0500

    17dec

--- 7dec/m.cpp
+++ 7dec/m.cpp
@@ -2,10 +2,13 @@
 
 using namespace std;
 
-int main(void) {
+int main(void)
+{
   int n, m;
 
   cin >> n >> m;
+
+  int **A = new int *[n];
 
   for (int i = 0; i < n; ++i)
     A[i] = new int[m];
@@ -14,9 +17,42 @@ int main(void) {
     for (int j = 0; j < m; ++j)
       cin >> A[i][j];
 
-  int maxRow[n], minColumn[m];
+  int maxRow[n], minColumn[m], max, min;
+
+  for (int i = 0; i < n; ++i)
+  {
+    maxRow[i] = 0;
+    max = A[i][0];
+
+    for (int j = 1; j < m; ++j)
+    {
+      if (A[i][j] > max)
+      {
+        max = A[i][j];
+        maxRow[i] = j;
+      }
+    }
+  }
+
+  for (int j = 0; j < m; ++j)
+  {
+    minColumn[j] = 0;
+    min = A[0][j];
+
+    for (int i = 1; i < n; ++i)
+    {
+      if (A[i][j] < min)
+      {
+        min = A[i][j];
+        minColumn[j] = i;
+      }
+    }
+  }
 
   for (int i = 0; i < n; ++i)
     for (int j = 0; j < m; ++j)
-
+      if (minColumn[j] == i && maxRow[i] == j) {
+        cout << i << " " << j << endl;
+        break;
+      }
 }
